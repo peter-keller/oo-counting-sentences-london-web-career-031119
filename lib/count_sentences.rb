a70ea0791b8a604b05ee94ee9e8a7c3aacab27e6
peter-keller <keller.f.peter@gmail.com>
2019-03-13 15:29:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    self.split(/\b?\.\s|\?\s|\!\s/).size
   end
 end
